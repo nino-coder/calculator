b4242838ce859d7b40a43ef47b85ce1ce39b02f2
nino-coder <nino.giorgadze.2000@gmail.com>
2020-05-04 00:10:41 +0400

    Updates

---
+++ precache-manifest.303eaed5d13c2b7f17df06e0f59924e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05beccf3014144f72dfda90fb934e266",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "006c87a6cb5e6802eee3",
+    "url": "/calculator/static/css/main.a0597cf7.chunk.css"
+  },
+  {
+    "revision": "8a6d84d71e70b8508950",
+    "url": "/calculator/static/js/2.7592b819.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.7592b819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006c87a6cb5e6802eee3",
+    "url": "/calculator/static/js/main.01309465.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
